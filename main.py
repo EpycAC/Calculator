1c1cbfc02a2d99582503ada7f93d6a8ba94bc230
Epyc <advay.rc@gmail.com>
2022-04-28 20:45:12 -0400

    added multiplication

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ def simplify(equation):
     c = list(b)
     return c
 
-def groupNum(equation):
+def groupNum(equ):
+    equation = simplify(equ)
     equationSimplified = []
     term = []
     for i in range(len(equation)):
@@ -29,12 +30,36 @@ def groupNum(equation):
             num = float("".join(term))
             equationSimplified.append(num)
     history.append(equationSimplified)
+    print(equationSimplified)
     return equationSimplified
 
 def solve(equation):
-    pass
+    terms = groupNum(equation)
+    moperator = []
+    aoperator = []
+    for i in range(len(terms)):
+        if (terms[i] == "*") or (terms[i] == "/"):
+            moperator.append([i, terms[i]])
+        elif (terms[i] == "+") or (terms[i] == "-"):
+            aoperator.append([i, terms[i]])
+    print(moperator)
+    print(aoperator)
+    indexn = 0
+    while len(moperator) > 0:
+        operator = moperator[0]
+        oindex = operator[0] - indexn
+        if operator[1] == "*":
+            newvalue = terms[oindex - 1] * terms[oindex + 1]
+            print(newvalue)
+        else:
+            newvalue = terms[oindex - 1] / terms[oindex + 1]
+            print(newvalue)
+    
+        moperator.remove(operator)
+    return 0
 
 equation = input("Enter what you want to solve: ")
+'''
 if equation.lower() == "history" or "h":
     adjustedHistory = history.reverse()
     for i in range(min(len(history), 10)):
@@ -43,5 +68,5 @@ if equation.lower() == "history" or "h":
             "".join(prev)
             print(prev)
 else:
-    equation = simplify(equation)
-    print(groupNum(equation))
+'''
+print(solve(equation))
